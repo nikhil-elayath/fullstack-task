74782b7e1eb40740f2ab9d2d7987ffb42a8c0c27
Nikhil Elayath <nikhil.elayath13@gmail.com>
2021-05-05 21:22:38 +0530

    styling of reuseable cards

--- fullstack-task/src/common/SearchCard.js
+++ fullstack-task/src/common/SearchCard.js
@@ -1,5 +1,12 @@
 import React from "react";
+import "../assests/styles/common/SearchCard.css"
 
 export default function SearchCard(props) {
-  return <div>SearchCard</div>;
+  return (
+    <div id="SearchCard__mainContainer">
+      <div id="SearchCard__imageContainer">Image</div>
+      <div id="SearchCard__headingContainer">Stanford</div>
+      <div id="SearchCard__descriptionContainer">SearchCard__descriptionContainer</div>
+    </div>
+  );
 }
--- fullstack-task/src/components/Homepage.js
+++ fullstack-task/src/components/Homepage.js
@@ -1,9 +1,20 @@
 import React from "react";
 import SearchCard from "../common/SearchCard";
+import  "../assests/styles/components/Homepage.css"
 
 export default function Homepage() {
   return (
-    <div>
+    <div id="Homepage__mainContainer">
+      <SearchCard />
+      <SearchCard />
+      <SearchCard />
+      <SearchCard />
+      <SearchCard />
+      <SearchCard />
+      <SearchCard />
+      <SearchCard />
+      <SearchCard />
+      <SearchCard />
       <SearchCard />
     </div>
   );
