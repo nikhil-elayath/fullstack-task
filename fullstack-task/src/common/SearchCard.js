ba2c7f66f743462cb4a577ab03875c43d265921c
Nikhil Elayath <nikhil.elayath13@gmail.com>
2021-05-08 13:14:33 +0530

    added media query for mobile responsiveness

--- fullstack-task/src/common/SearchCard.js
+++ fullstack-task/src/common/SearchCard.js
@@ -1,12 +1,21 @@
 import React from "react";
-import "../assests/styles/common/SearchCard.css"
+import "../assests/styles/common/SearchCard.css";
 
 export default function SearchCard(props) {
   return (
     <div id="SearchCard__mainContainer">
-      <div id="SearchCard__imageContainer">{props.universityImage}</div>
+      <div id="SearchCard__imageContainer">
+        <img
+          id="SearchCard__image"
+          src={
+            "https://upload.wikimedia.org/wikipedia/en/thumb/6/6c/Abilene_Christian_Wildcats_logo.svg/1200px-Abilene_Christian_Wildcats_logo.svg.png"
+          }
+        ></img>
+      </div>
       <div id="SearchCard__headingContainer">{props.universityName}</div>
-      <div id="SearchCard__descriptionContainer">{props.universityDescription}</div>
+      <div id="SearchCard__descriptionContainer">
+        {props.universityDescription}
+      </div>
       <div id="SearchCard__descriptionContainer">{props.webpageUrl}</div>
     </div>
   );
